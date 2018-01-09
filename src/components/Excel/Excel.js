e20bf9db87cc4b7fa7cadde8929f918ef63a0696
rajesh.pillai <rajesh.pillai@kohls.com>
2018-01-09 03:16:38 -0800

    Feature: Added sorting clue

--- src/components/Excel/Excel.js
+++ src/components/Excel/Excel.js
@@ -8,23 +8,33 @@ export default class extends React.Component {
 
     state = {
         headers: this.props.model.headers,
-        data: this.props.model.data
+        data: this.props.model.data,
+        sortby: null,
+        descending: false
     }
 
     _sort=(e) => {
         var data = this.state.data;
         var column = e.target.cellIndex;
+        var descending = this.state.sortby === column && !this.state.descending;
         data.sort((a,b) => {
-            return a[column] > b[column];
+            return descending
+                ? a[column] < b[column]
+                :a[column] > b[column];
         });
 
         this.setState({
-            data: data
+            data: data,
+            sortby: column,
+            descending
         });
     }
     render() {
         var {headers,data} = this.state;
-        var headerView = headers.map((header) => {
+        var headerView = headers.map((header, index) => {
+            if (this.state.sortby === index) {
+                header += this.state.descending ? '\u2191': '\u2193'
+            }
             return (
                 <th>
                     {header}
