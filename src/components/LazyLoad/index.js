c3c7c31456f5f0b930fbe710fb018902ee65353e
Rajesh Pillai <pillai.rajesh@gmail.com>
2018-01-16 10:01:44 +0530

    Added index on LazyLoad

--- src/components/LazyLoad/index.js
+++ src/components/LazyLoad/index.js
@@ -3,8 +3,8 @@ import LazyComponent from './LazyComponent';
 
 export default class LazyLoad extends React.Component {
     render() {
-        var data = this.props.data.map((d) => {
-            return <LazyComponent data={d} />
+        var data = this.props.data.map((d,i) => {
+            return <LazyComponent key={i} data={d} />
         });
         return (
             <div>
