9be620b0381b198d722734f24884543eef066934
rajesh.pillai <rajesh.pillai@kohls.com>
2018-01-19 04:39:40 -0800

    Added month selection, utils for mousemove, and css

--- src/components/Calendar/index.js
+++ src/components/Calendar/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import moment from 'moment';
 import './calendar.css';
+import './utils.js';
 
 export default class Calendar extends React.Component {
     constructor() {
---
+++ src/components/Calendar/utils.js
@@ -0,0 +1,26 @@
+const getPosition = (el) => {
+    var xPosition = 0;
+    var yPosition = 0;
+   
+    while (el) {
+      if (el.tagName == "BODY") {
+        // deal with browser quirks with body/window/document and page scroll
+        var xScrollPos = el.scrollLeft || document.documentElement.scrollLeft;
+        var yScrollPos = el.scrollTop || document.documentElement.scrollTop;
+   
+        xPosition += (el.offsetLeft - xScrollPos + el.clientLeft);
+        yPosition += (el.offsetTop - yScrollPos + el.clientTop);
+      } else {
+        xPosition += (el.offsetLeft - el.scrollLeft + el.clientLeft);
+        yPosition += (el.offsetTop - el.scrollTop + el.clientTop);
+      }
+   
+      el = el.offsetParent;
+    }
+    return {
+      x: xPosition,
+      y: yPosition
+    };
+  }
+
+  export default getPosition;
