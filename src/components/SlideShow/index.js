9f41bd9bf5dd9964a05f95dd30c2089fe3b8fb03
Rajesh Pillai <pillai.rajesh@gmail.com>
2018-02-26 12:07:26 +0530

    Refactor: Slideshow wrap with modal component

--- src/components/SlideShow/index.js
+++ src/components/SlideShow/index.js
@@ -1,11 +1,14 @@
 import React from 'react';
 import Slide from './Slide'
+import withModal from '../HOC/withModal';
+
 import './slideshow.css';
 
 export default class SlideShow extends React.Component {
     render () {
+        var SlideWithModal = withModal(Slide);
         let slides = this.props.slides.map((slide) => {
-            return <Slide  key = {slide.id} data = {slide} />
+            return <SlideWithModal  key = {slide.id} data = {slide} />
         })
         return (
             <div className="slide-container">
